664e919e1d31c9dd661d5ec435f0a53185fa120b
zaluru <zaluru@tutamail.com>
2023-01-21 13:58:56 +0100

    Changed code highlighting color theme to dracula

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,20 @@ import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), mdx()]
+  integrations: [tailwind(), svelte(), mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'dracula',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+    },
+  },
 });
