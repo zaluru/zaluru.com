c7ce263abf8e0277b3374822c3175ca500c46081
zaluru <zaluru@zaluru.com>
2024-03-04 01:08:03 +0100

    feat: sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://zaluru.com",
   integrations: [tailwind(), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
